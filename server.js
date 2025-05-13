358797bb97bd795f3a7abf3cfd6bd21bf16369af
Murillo Godoy <156554319+murillogodoy@users.noreply.github.com>
2025-05-13 19:33:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,15 +23,33 @@ app.get("/", async (req, res) => {
 
 app.get("/new", (req, res) => {
     try {
-        res.render("modify.ejs", {header: "New Post" });
+        res.render("modify.ejs", {header: "New Post", submit: "New Post" });
     } catch (error) {
         res.status(404).json({ message: "Cannot access site"});
     }
 });
 
-app.post("/newPost", async (req, res) => {
-    const response = await axios.post(`${API_URL}/newPost`, req.body);
+app.get("/edit/:id", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+        console.log(response.data);
+        res.render("modify.ejs", {header: "Edit Post", submit: "Update Post", post: response.data, });
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching post"});
+    }
 });
+
+app.post("/newPost", async (req, res) => {
+    try {
+        const response = await axios.post(`${API_URL}/newPost`, req.body);
+        console.log(response);
+        res.redirect("/");
+    } catch (error) {
+        res.status(500).json({ message: "Error creating post" });
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Backend server is running on http://localhost:${port}`);
